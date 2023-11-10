1f5d88bd350112ff3a7bd1587cd5c06f08a27e98
Jordan <79342877+Jordan231111@users.noreply.github.com>
2023-11-10 16:18:39 -0500

    Fix react packages
    Try to implement accordion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+//import './styles/.css';
+import React from 'react';
+import Accordion from './components/accordion';
 
-function App() {
+
+export default function app() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello CodeSandbox</h1>
+      <Accordion />
     </div>
   );
-}
 
-export default App;
+}
